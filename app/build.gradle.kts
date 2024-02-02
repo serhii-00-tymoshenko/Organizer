b0981985cba930991354e6b8b37c0c9d4b0f749c
serhii <serhii.tymoshenko@yahoo.com>
2024-02-02 11:11:34 +0100

    Initial setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -25,7 +26,16 @@ android {
                 "proguard-rules.pro"
             )
         }
+        debug {
+            buildConfigField("String", "WEB_CLIENT_ID", project.properties["WEB_CLIENT_ID"].toString())
+        }
     }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -44,4 +54,31 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // ViewModel lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+
+    // Firebase cloud messaging
+    implementation("com.google.firebase:firebase-messaging-ktx")
+
+    // Firebase firestore
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
+    // Firebase auth
+    implementation("com.google.firebase:firebase-auth-ktx")
+
+    // Koin
+    implementation("io.insert-koin:koin-android:3.5.3")
+
+    // Coil
+    implementation("io.coil-kt:coil:2.5.0")
+
+    // Google Play services
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
 }
